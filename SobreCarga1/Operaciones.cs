d1eb885249be2d9e549fe4177cf445823b36e1d7
García Rodríguez Alan Humberto <54951428+AlanSennin@users.noreply.github.com>
2019-09-25 01:00:19 -0700

    Sobrecarga de Metodos
    Sobrecarga de un Metodo Multiplicar.

---
+++ SobreCarga1/Operaciones.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SobreCarga1
+{
+    public class Operaciones
+    {
+        public double Multiplicar() //Metodo sin Parametros
+        {
+            double Res;
+
+            Res = 0;
+
+            return Res;
+        }
+
+        public double Multiplicar(double A) //Metodo con 1 Parametro
+        {
+            double Res;
+
+            Res = A;
+
+            return Res;  
+        }
+
+        public double Multiplicar(double B, double C) //Metodo con 2 Parametros
+        {
+            double Res;
+
+            Res = B * C;
+
+            return Res;
+        }
+    }
+}
